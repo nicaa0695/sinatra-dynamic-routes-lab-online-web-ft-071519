7a2a0120160ca500ed5b2edf31f48c43b8ba2d98
Veronica Lopez <nicaa0695@gmail.com>
2019-08-27 19:19:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,7 +29,11 @@ class App < Sinatra::Base
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end 
   
-  get '/operation/:number1/:number2' do 
+  get '/:operation/:number1/:number2' do 
+    @oper = params[:oper]
+    @num1 = params[:num1]
+    @num2 = params[:num2]
+    
     
   
 
