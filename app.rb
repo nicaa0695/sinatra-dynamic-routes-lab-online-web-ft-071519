ef97b84efee4f695427584a856b3d08ee1b3f524
Veronica Lopez <nicaa0695@gmail.com>
2019-08-27 19:12:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,13 @@ class App < Sinatra::Base
     @num.to_s
   end 
   
-  
+  get '/say/:number/:phrase' do
+    str = ""
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    @num.times { str += "#{@phrase}\n" }
+    str
+  end
   
 
 end
