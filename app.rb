ad3f8c258be8e2f4b3ef6c07b48d6b6cf073bd88
Veronica Lopez <nicaa0695@gmail.com>
2019-08-27 19:21:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -33,7 +33,17 @@ class App < Sinatra::Base
     @oper = params[:operation]
     @num1 = params[:number1].to_i 
     @num2 = params[:number2].to_i 
-    
+    case @oper
+      when "subtract"
+        (@num1 - @num2).to_s
+      when "add"
+        (@num1 + @num2).to_s
+      when "multiply"
+        (@num1 * @num2).to_s
+      when "divide"
+        (@num1 / @num2).to_s
+    end
+  end
     
     
   
